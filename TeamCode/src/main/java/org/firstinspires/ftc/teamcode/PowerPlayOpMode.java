8c922e4ae185bd361ad3a6025cf29958d7990802
Adar <adar.wasserman@gmail.com>
2022-08-31 11:12:49 +0300

    Moved driver code into its own class.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/PowerPlayOpMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/PowerPlayOpMode.java
@@ -7,23 +7,18 @@ import com.arcrobotics.ftclib.hardware.motors.Motor;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
+import org.firstinspires.ftc.teamcode.TeleOpDrive.Drive;
 import org.firstinspires.ftc.teamcode.TeleOpDrive.imu.IMU;
 
 @TeleOp
 public class PowerPlayOpMode extends LinearOpMode {
 
-    enum DriveMode {
-        JokerDrive,
-        RegularDrive
-    }
 
     private Motor frontLeft, frontRight, backLeft,backRight;
-    private MecanumDrive drive;
-    private GamepadEx driverOp;
+    private Drive drive;
+    private GamepadEx driver;
 
-    private DriveMode mode = DriveMode.JokerDrive;
 
-    private double powerMultiplier = 1;
 
     @Override
     public void runOpMode() throws InterruptedException {
@@ -33,52 +28,20 @@ public class PowerPlayOpMode extends LinearOpMode {
         backLeft = new Motor(hardwareMap,"backLeft");
         backRight = new Motor(hardwareMap,"backRight");
 
-        drive = new MecanumDrive(frontLeft,
-                                    frontRight,
-                                        backLeft,
-                                            backRight);
-        driverOp = new GamepadEx(gamepad1);
+
+        driver = new GamepadEx(gamepad1);
 
 
         IMU imu = new IMU(hardwareMap);
 
+        drive = new Drive(driver,imu,telemetry,frontLeft,frontRight,backLeft,backRight);
+
         waitForStart();
 
-        double heading = 0;
 
         while(opModeIsActive()){
-            changePowerByRightBumper();
-
-            if(mode == DriveMode.JokerDrive)
-                heading = imu.getHeading();
-
-            if(mode == DriveMode.JokerDrive) {
-                drive.driveFieldCentric(
-                        -driverOp.getLeftX() * powerMultiplier, //strafe
-                        -driverOp.getLeftY() * powerMultiplier, //forward
-                        -driverOp.getRightX() * powerMultiplier, //turn
-                        heading //angle
-
-                );
-            }else{
-                drive.driveRobotCentric(
-                        -driverOp.getLeftX() * powerMultiplier, //strafe
-                        -driverOp.getLeftY() * powerMultiplier, //forward
-                        -driverOp.getRightX() * powerMultiplier //turn
-                );
-            }
-
-            telemetry.addData("Heading", heading);
-            telemetry.update();
+            drive.update();
         }
 
-    }
-
-    private void changePowerByRightBumper(){
-        powerMultiplier = driverOp.isDown(GamepadKeys.Button.RIGHT_BUMPER) ? 0.5 : 1;
-    }
-
-    private void changeModeByLeftBumper(){
-        mode = driverOp.wasJustPressed(GamepadKeys.Button.LEFT_BUMPER) ? mode  == DriveMode.JokerDrive ? DriveMode.RegularDrive : DriveMode.JokerDrive : mode;
     }
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/opmode/BackAndForth.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/opmode/BackAndForth.java
@@ -2,6 +2,7 @@ package org.firstinspires.ftc.teamcode.drive.opmode;
 
 import com.acmerobotics.dashboard.config.Config;
 import com.acmerobotics.roadrunner.geometry.Pose2d;
+import com.acmerobotics.roadrunner.geometry.Vector2d;
 import com.acmerobotics.roadrunner.trajectory.Trajectory;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
@@ -34,19 +35,15 @@ public class BackAndForth extends LinearOpMode {
     public void runOpMode() throws InterruptedException {
         SampleMecanumDrive drive = new SampleMecanumDrive(hardwareMap);
 
-        Trajectory trajectoryForward = drive.trajectoryBuilder(new Pose2d())
-                .forward(DISTANCE)
+        Trajectory trajectorySplineTo = drive.trajectoryBuilder(new Pose2d(15,15, 0))
+                .splineTo(new Vector2d(30,15),90)
                 .build();
 
-        Trajectory trajectoryBackward = drive.trajectoryBuilder(trajectoryForward.end())
-                .back(DISTANCE)
-                .build();
 
         waitForStart();
 
         while (opModeIsActive() && !isStopRequested()) {
-            drive.followTrajectory(trajectoryForward);
-            drive.followTrajectory(trajectoryBackward);
+            drive.followTrajectory(trajectorySplineTo);
         }
     }
 }
